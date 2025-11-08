f0e98f5c0daa7b9a33e5f74180e09c4bbf2b913b
rworsham <robertworsham01@gmail.com>
2025-11-08 16:56:09 -0500

    Adjusted formatting of balance on CategoryCardList.kt

--- composeApp/src/commonMain/kotlin/com/budgetbud/kmp/ui/components/CategoryCardList.kt
+++ composeApp/src/commonMain/kotlin/com/budgetbud/kmp/ui/components/CategoryCardList.kt
@@ -32,7 +32,7 @@ fun CategoryCardList(
                     )
                     Spacer(modifier = Modifier.height(4.dp))
                     Text(
-                        text = "Balance: ${category.balance}",
+                        text = "Balance: $${"%.2f".format(category.balance)}",
                         style = MaterialTheme.typography.bodyMedium
                     )
                 }
