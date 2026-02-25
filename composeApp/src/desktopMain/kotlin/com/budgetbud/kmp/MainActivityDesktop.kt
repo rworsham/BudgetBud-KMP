1f623023a1f130ad06a7c1b873c443c40c4316db
rworsham <robertworsham01@gmail.com>
2026-02-24 21:42:23 -0500

    Set minimum window size on desktop ver

--- composeApp/src/desktopMain/kotlin/com/budgetbud/kmp/MainActivityDesktop.kt
+++ composeApp/src/desktopMain/kotlin/com/budgetbud/kmp/MainActivityDesktop.kt
@@ -2,12 +2,17 @@ package com.budgetbud.kmp
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
 
 fun main() = application {
+    val windowState = rememberWindowState()
     Window(
         onCloseRequest = ::exitApplication,
         title = "BudgetBud",
+        state = windowState
     ) {
+        window.minimumSize = Dimension(1000, 800)
         App()
     }
 }
