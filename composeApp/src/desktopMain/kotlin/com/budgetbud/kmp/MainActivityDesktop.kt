8b280895e084815910266af8df74eb29f6f54532
rworsham <robertworsham01@gmail.com>
2026-03-25 19:12:45 -0400

    Added larger starting window size

--- composeApp/src/desktopMain/kotlin/com/budgetbud/kmp/MainActivityDesktop.kt
+++ composeApp/src/desktopMain/kotlin/com/budgetbud/kmp/MainActivityDesktop.kt
@@ -1,12 +1,19 @@
 package com.budgetbud.kmp
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import java.awt.Dimension
 
 fun main() = application {
-    val windowState = rememberWindowState()
+    val windowState = rememberWindowState(
+        width = 1400.dp,
+        height = 900.dp,
+        position = WindowPosition(Alignment.Center)
+    )
     Window(
         onCloseRequest = ::exitApplication,
         title = "BudgetBud",
